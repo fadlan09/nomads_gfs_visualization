0ccafaed0ad50354e1482b4275d3631df6e63bf3
fadlan09 <afadlan0@gmail.com>
2025-07-03 11:18:19 +0700

    Update appy.py

--- appy.py
+++ appy.py
@@ -25,10 +25,10 @@ run_date = st.sidebar.date_input("Tanggal Run GFS (UTC)", today.date())
 run_hour = st.sidebar.selectbox("Jam Run GFS (UTC)", ["00", "06", "12", "18"])
 forecast_hour = st.sidebar.slider("Jam ke depan", 0, 240, 0, step=1)
 parameter = st.sidebar.selectbox("Parameter", [
-    "Curah Hujan per jam",
-    "Suhu Permukaan",
-    "Angin Permukaan",
-    "Tekanan Permukaan Laut"
+    "Curah Hujan per jam (pratesfc)",
+    "Suhu Permukaan (tmp2m)",
+    "Angin Permukaan (ugrd10m & vgrd10m)",
+    "Tekanan Permukaan Laut (prmslmsl)"
 ])
 
 if st.sidebar.button("🔎 Tampilkan Visualisasi"):
